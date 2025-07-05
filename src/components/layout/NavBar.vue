096c33bd61aa26e30ba09768433bf6b35c86f9cc
Grigore Gabriel Trifan <trifangaby71@protonmail.com>
2025-07-05 03:46:26 +0300

    minor flex change

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -13,29 +13,31 @@ const isLoginPage = computed(() => route.path === '/login');
 
 <template>
   <header class="sticky top-0 z-50 w-full bg-zinc-900/95 text-white shadow-md backdrop-blur supports-[backdrop-filter]:bg-zinc-900/60">
-    <div class="container flex h-16 items-center justify-between">
-      <div class="flex items-center space-x-4">
-        <RouterLink to="/" class="flex items-center space-x-2 ml-4 hover:text-zinc-200 transition-colors">
+    <div class="container mx-auto px-4 h-16 flex items-center justify-between">
+      <div class="flex-1">
+        <RouterLink to="/" class="inline-flex items-center hover:text-zinc-200 transition-colors">
           <span class="font-bold text-xl">StoreSkills</span>
         </RouterLink>
       </div>
       
-      <div v-if="!isLoginPage" class="flex items-center space-x-2 mr-4">
-        <RouterLink v-if="!isAuthenticated" to="/login">
-          <Button variant="ghost" class="text-white/90 hover:bg-zinc-800/80 hover:text-white">
-            <LogIn class="mr-2 h-4 w-4" />
-            <span>Login</span>
+      <div v-if="!isLoginPage" class="flex items-center justify-end flex-1">
+        <div class="flex space-x-2">
+          <RouterLink v-if="!isAuthenticated" to="/login" class="flex items-center">
+            <Button variant="ghost" class="text-white/90 hover:bg-zinc-800/80 hover:text-white">
+              <LogIn class="mr-2 h-4 w-4" />
+              <span>Login</span>
+            </Button>
+          </RouterLink>
+          <Button
+            v-if="isAuthenticated"
+            variant="ghost"
+            class="text-white/90 hover:bg-zinc-800/80 hover:text-white"
+            @click="logout"
+          >
+            <LogOut class="mr-2 h-4 w-4" />
+            <span>Logout</span>
           </Button>
-        </RouterLink>
-        <Button
-          v-if="isAuthenticated"
-          variant="ghost"
-          class="text-white/90 hover:bg-zinc-800/80 hover:text-white"
-          @click="logout"
-        >
-          <LogOut class="mr-2 h-4 w-4" />
-          <span>Logout</span>
-        </Button>
+        </div>
       </div>
     </div>
   </header>
